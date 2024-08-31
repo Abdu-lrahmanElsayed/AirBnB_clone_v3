0afe2df0c9fce57f2bc65fbed31b0198c848d957
Abdu <Abdu>
2024-08-31 06:21:04 -0700

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""index"""
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")

673fd47dbd9433aa25c8e078c47d8b555189bf64
Abdu <Abdu>
2024-08-31 05:46:14 -0700

    task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,26 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID"""
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for v in all_cls.values():
+            if value.id == id:
+                return value
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage
+        matching the given class.
+        If no class is passed,
+        returns the count of all objects in storage.
+        """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
